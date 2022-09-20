7d485b3cdc8b5b88b9ea377ae1a83c2f6bffac4e
Bilal Abbas <babbas0505@gmail.com>
2022-09-20 20:04:20 +0100

    included api data for orders

--- server/api/orders.js
+++ server/api/orders.js
@@ -1,17 +1,24 @@
 const router = require('express').Router();
+const { Order } = require('../db');
 
-router.get('/orders', (req, res, next) => {
+router.get('/orders', async (req, res, next) => {
     try {
-        res.send('all orders')
+        const allOrders = await Order.findAll({});
+        res.send(allOrders);
     } catch (error) {
         next(error)
     }
 })
 
-router.get('/orders/:id', (req, res, next) => {
+router.get('/orders/:id', async (req, res, next) => {
     try {
         const id = req.params.id
-        res.send(`single order: order ${id}`)
+        const singleOrder = await Order.findByPk(id);
+    if (singleOrder) {
+      res.send(singleOrder);
+    } else {
+      res.send('error: no order available');
+    }
     } catch (error) {
         next(error)
     }
