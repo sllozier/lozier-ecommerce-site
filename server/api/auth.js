7424f8f73e1e13cdcf9e97988c1030dea3906284
wsiddons <wsiddons11@gmail.com>
2022-09-21 16:41:55 -0400

    added styles to sign up - does not function... seems like an error with the way the axios resquests are set up

--- server/api/auth.js
+++ server/api/auth.js
@@ -1,31 +1,31 @@
 const router = require('express').Router();
-const Account = require('./database');
+const Account = require('../db/Account');
 
-router.post('/signup', async( req, res, next) => {
-  try{
+router.post('/signup', async (req, res, next) => {
+  try {
     const account = await Account.create(req.body);
-    if(!account) res.sendStatus(404);
+    if (!account) res.sendStatus(404);
     const token = await user.generateToken()
     res.send(token);
-  }catch(error){
+  } catch (error) {
     next(error)
   }
 });
 
-router.post('/login', async(req, res, next) => {
-  try{
+router.post('/login', async (req, res, next) => {
+  try {
     const token = await Account.authenticate(req.body);
     res.send(token);
-  }catch(error){
+  } catch (error) {
     next(error);
   }
 });
 
-router.get('/authuser', async(req, res, next) => {
-  try{
+router.get('/authuser', async (req, res, next) => {
+  try {
     const authUser = await Account.findByToken(req.headers.authorization);
     res.send(authUser);
-  }catch(error){
+  } catch (error) {
     next(error)
   }
 })
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -5,7 +5,7 @@ import { signup } from '../../store/reducers1/authReducer';
 
 const SignUp = () => {
     const dispatch = useDispatch();
-    const [ account, setAccount ] = useState({
+    const [account, setAccount] = useState({
         firstName: '',
         lastName: '',
         email: '',
@@ -14,31 +14,30 @@ const SignUp = () => {
         confirmPassword: '',
     })
 
-    const [ hideRequired, setHideRequired ] = useState(true);
+    const [hideRequired, setHideRequired] = useState(true);
 
     const handleSubmit = (event) => {
         event.preventDefault();
-
-        if(account.password === account.confirmPassword){
+        if (account.password === account.confirmPassword) {
             let trigger = true;
             let addAccount = {};
-            for (let key in account){
-                if(account[key]){
-                    if(key !== 'confirmPassword'){
+            for (let key in account) {
+                if (account[key]) {
+                    if (key !== 'confirmPassword') {
                         addAccount[key] = account[key]
                     }
                 }
             }
-            if (trigger){
-                dispatch(signup({...account}))
-            }else{
+            if (trigger) {
+                dispatch(signup({ ...account }))
+            } else {
                 setHideRequired(false)
-                setAccount({...account, password: '', confirmPassword: ''})
+                setAccount({ ...account, password: '', confirmPassword: '' })
                 alert('Fill in all required fields')
             }
-        }else {
+        } else {
             alert('Enter matching password')
-            setAccount({...account, password:'', confirmPassword: ''})
+            setAccount({ ...account, password: '', confirmPassword: '' })
         }
     }
 
@@ -46,53 +45,54 @@ const SignUp = () => {
     //I opted to do it this way.
 
     return (
-        <div id='account-signup'>
-            <h2>Sign up here:</h2>
-        <form onSubmit={handleSubmit}>
-            <label htmlFor='firstName'>First Name: {hideRequired ? '' : <span className='required'>first name required</span>}</label>
-            <input 
-            name='firstName' 
-            value={account.firstName} 
-            onChange={(event) => setAccount({...account, firstName: event.target.value})}/>
+        <>
+            <div id='account-signup' className='signup-container'>
+                <h2>Sign up here:</h2>
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor='firstName'>First Name: {hideRequired ? '' : <span className='required'>first name required</span>}</label>
+                    <input
+                        name='firstName'
+                        value={account.firstName}
+                        onChange={(event) => setAccount({ ...account, firstName: event.target.value })} />
 
-            <label htmlFor='lastName'>Last Name: {hideRequired ? '' : <span className='required'>last name required</span>}</label>
-            <input 
-            name='lastName' 
-            value={account.lastName} 
-            onChange={(event) => setAccount({...account, lastName: event.target.value})}/>
+                    <label htmlFor='lastName'>Last Name: {hideRequired ? '' : <span className='required'>last name required</span>}</label>
+                    <input
+                        name='lastName'
+                        value={account.lastName}
+                        onChange={(event) => setAccount({ ...account, lastName: event.target.value })} />
 
-            <label htmlFor='email'>Email: {hideRequired ? '' : <span className='required'>email required</span>}</label>
-            <input 
-            name='email'
-            type='email'
-            value={account.email} 
-            onChange={(event) => setAccount({...account, email: event.target.value})}/>
+                    <label htmlFor='email'>Email: {hideRequired ? '' : <span className='required'>email required</span>}</label>
+                    <input
+                        name='email'
+                        type='email'
+                        value={account.email}
+                        onChange={(event) => setAccount({ ...account, email: event.target.value })} />
 
-            <label htmlFor='username'>Choose a username: {hideRequired ? '' : <span className='required'>username required</span>}</label>
-            <input 
-            name='username' 
-            value={account.username} 
-            onChange={(event) => setAccount({...account, username: event.target.value})}/>
+                    <label htmlFor='username'>Choose a username: {hideRequired ? '' : <span className='required'>username required</span>}</label>
+                    <input
+                        name='username'
+                        value={account.username}
+                        onChange={(event) => setAccount({ ...account, username: event.target.value })} />
 
-            <label htmlFor='password'>Password: {hideRequired ? '' : <span className='required'>password required</span>}</label>
-            <input 
-            name='password'
-            type='password' 
-            value={account.password} 
-            onChange={(event) => setAccount({...account, password: event.target.value})}/>
+                    <label htmlFor='password'>Password: {hideRequired ? '' : <span className='required'>password required</span>}</label>
+                    <input
+                        name='password'
+                        type='password'
+                        value={account.password}
+                        onChange={(event) => setAccount({ ...account, password: event.target.value })} />
 
-            <label htmlFor='confirmPassword'>Confirm password: {hideRequired ? '' : <span className='required'>password confirmation required</span>}</label>
-            <input 
-            name='confirmPassword'
-            type='password' 
-            value={account.confirmPassword} 
-            onChange={(event) => setAccount({...account, confirmPassword: event.target.value})}/>
+                    <label htmlFor='confirmPassword'>Confirm password: {hideRequired ? '' : <span className='required'>password confirmation required</span>}</label>
+                    <input
+                        name='confirmPassword'
+                        type='password'
+                        value={account.confirmPassword}
+                        onChange={(event) => setAccount({ ...account, confirmPassword: event.target.value })} />
 
-            <button type='submit'>Submit</button>
-            <button type='button'><Link to='/'>Cancel</Link></button>
-        </form>
-
-        </div>
+                    <button type='submit'>Submit</button>
+                    {/* <button type='button'><Link to='/'>Cancel</Link></button> */}
+                </form>
+            </div>
+        </>
     )
 }
 
