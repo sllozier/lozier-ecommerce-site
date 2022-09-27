2b9e3e21007c7a6152b5d15515debdf7cf9e7da1
Bilal Abbas <babbas0505@gmail.com>
2022-09-27 18:03:41 +0100

    added styling

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -25,21 +25,33 @@ function NavBar() {
                     <h3>Cart</h3>
                 </Link> */}
 
-                <Link to='/products'>
-                    <h3>Home</h3>
+                <Link to='/products' style={{ textDecoration: 'none' }}>
+                <div className="logoContainer">
+                    <img src="vinyl_logo.png" className="navBarLogo" alt="vinyl icon image" />
+                    <h3 id="logoText">Flintstones Album Collective</h3>
+                </div>
                 </Link>
-                <Link to='/about'>
+                <Link to='/products' style={{ textDecoration: 'none' }}>
+                    <h3>Albums</h3>
+                </Link>
+                <Link to='/about' style={{ textDecoration: 'none' }}>
                     <h3>About</h3>
                 </Link>
-                <Link to='account-nav'>
+                <div className="navBarShop">
+                <Link to='account-nav' style={{ textDecoration: 'none' }}>
                     <h3>Sign In</h3>
                 </Link>
-                <Link to='account-nav/signup'>
+                <Link to='account-nav/signup' style={{ textDecoration: 'none' }}>
                     <h3>Sign Up</h3>
                 </Link>
-                <Link to='cart'>
-                    <h3>Cart</h3>
+
+                <Link to='cart' style={{ textDecoration: 'none' }}>
+                <div className="logoContainer">
+                <img className="navBarLogo" src="cart_logo.png" alt="" />
+                    <h3 id="logoText">Cart</h3>
+                </div>
                 </Link>
+                </div>
             </nav>
         </>
     )
