32f7e73f2f6a15812410521ef721f9257e738db5
Bilal Abbas <babbas0505@gmail.com>
2022-09-28 18:42:51 +0100

    more fixes

--- src/components/NavBar/SignInOut.js
+++ src/components/NavBar/SignInOut.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { logoutAccount } from "../../store/reducers1/authReducer";
 
 const SignInOut = () => {
   const account = useSelector((state) => state.account);
 
+  const logout = () => {
+    dispatch(logoutAccount());
+    navigate('/');
+};
+
   return account.username ? (
-    <Link to="/products" style={{ textDecoration: 'none' }}>
+    <Link onClick={logout} to="/" style={{ textDecoration: 'none' }}>
       <h3>Sign Out</h3>
     </Link>
   ) : (
