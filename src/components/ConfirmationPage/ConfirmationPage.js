29e7f28c0c1157eddff6aecd72074b8b7c607ea6
wsiddons <wsiddons11@gmail.com>
2022-09-28 13:10:39 -0400

    still chugging;

--- src/components/ConfirmationPage/ConfirmationPage.js
+++ src/components/ConfirmationPage/ConfirmationPage.js
@@ -7,12 +7,15 @@ const ConfirmationPage = () => {
     const min = 100000;
 
     let orderNumber = Math.floor(Math.random() * (max - min + 1) + min);
-    
-    return(
-        <div className='confirmation-page'>
-            <h1>Thank you for your order! We hope you enjoy listening!</h1>
+
+    return (
+        <div className='confirmation-page-container'>
+            <h1>Thank you for your order!</h1>
+            <h1>We hope you enjoy listening!</h1>
             <h3>{`Order Number: #${orderNumber}`}</h3>
-            <Link to='/products'>Back to Homepage</Link>
+            <Link to='/products'>
+                <button>Back to Homepage</button>
+            </Link>
         </div>
     )
 };
