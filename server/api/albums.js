51322b8d95720c2c0bba19f68e55f3b3baded7b0
sllozier <sarah.lozier@gmail.com>
2022-11-30 20:17:56 -0500

    working on orderslice

--- server/api/albums.js
+++ server/api/albums.js
@@ -26,5 +26,16 @@ router.get('/albums/:id', async (req, res, next) => {
   }
 });
 
+router.get('/artist/:id', async(req, res, next) => {
+  try{
+    const singleArtist = await Artist.findByPk(req.params.id, {
+      include: Product,
+    });
+    res.send(singleArtist);
+  }catch(error){
+    next(error);
+  }
+});
+
 
 module.exports = router;
--- src/store/reducers/orderSlice.js
+++ src/store/reducers/orderSlice.js
@@ -1,19 +1,45 @@
 import { createSlice } from "@reduxjs/toolkit";
+import * as bulmaToast from 'bulma-toast';
 import axios from 'axios';
+
+bulmaToast.setDefaults({
+    position:'top-center',
+    duration: 1600,
+    pauseOnHover: true,
+    type: 'is-warning',
+    animate: {in: 'fadeIn'},
+})
+
+
 
 const orderSlice = createSlice({
     name: "orderSlice",
     initialState: {
         orderList: [],
         orderData: {},
+        lineItems: 0,
     },
     reducers: {
+        setOrderData: (state, action) => {
+            return action.payload;
+        },
         getOrderList: (state, action) => {
             state.orderList = action.payload;
             return state;
         },
         getOrderData: (state, action) => {
             state.orderData = action.payload;
+            return state;
+        },
+        addItem: (state) => {
+            state.lineItems += 1;
+        },
+        removeItem: (state, action) => {
+            state.lineItems = action.payload;
+            return state;
+        },
+        _updateQuantity: (state, action) => {
+            state.lineItems = action.payload;
             return state;
         },
         setErrorMsg: (state, action) => {
@@ -25,27 +51,134 @@ const orderSlice = createSlice({
 
 export default orderSlice.reducer;
 export const {
+    setOrders,
     getOrderList,
     getOrderData,
+    addItem,
+    removeItem,
+    _updateQuantity,
     setErrorMsg,
 } = orderSlice.actions;
 
 //thunks go here//
 
-export const fetchOrderList = () => async(dispatch) => {
+export const fetchOrderList = accountId => async(dispatch) => {
     try{
-        const { data: orderList } = await axios.get("/api/orders");
-        dispatch(getOrderList(orderList));
+        const token = window.localStorage.getItem('token');
+        const order = window.localStorage.getItem('order');
+        if(token){
+            const { data: orderList } = await axios.get(`/api/orders/${accountId}`, {
+                headers: {
+                    authorization: token,
+                },
+            });
+            dispatch(setOrders(orderList));
+            if (order) {
+                JSON.parse(order).lineItems.forEach(item => {
+                    dispatch(addOrderItem(item.product.id));
+                });
+                window.localStorage.removeItem('order');
+            }
+        }else{
+            if(!order)
+            window.localStorage.setItem('order',
+            JSON.stringify({
+                complete: false,
+                lineItems: [],
+            })
+            );
+        }        
     }catch(error){
-        console.log("FETCH ORDER LIST ERROR", error);
+        console.error("FETCH ORDER LIST ERROR", error);
     }
 };
 
-export const fetchOrderData = (orderId) => async(dispatch) => {
+export const addOrderItem = productId => async(dispatch) => {
     try{
-        const { data: orderData } = await axios.get(`/api/orders/${orderId}`);
-        dispatch(getOrderData(orderData));
+        const token = window.localStorage.getItem('token');
+        if(token){
+            const { data: itemData } = await axios.put(`/api/orders/${productId}`, {}, {
+                headers: {
+                    authorization: token,
+                },
+            });
+            dispatch(addItem(itemData));
+        }else{
+            const order = window.localStorage.getItem('order');
+            const { data: itemData } = await axios.get(`/api/albums/${productId}`);
+            const newOrder = JSON.parse(order);
+            if(newOrder.lineItems.some(item => item.product.id === itemData.id))
+            throw new Error('Item already exists in order');
+        const item = {
+            id: newOrder.lineItems.length + 1,
+            quantity: 1,
+            product: itemData,
+        };
+        newOrder.lineItems.push(item);
+        localStorage.order = JSON.stringify(newOrder);
+        dispatch(addItem(item));
+        }
     }catch(error){
-        console.log("FETCH ORDER DATA ERROR", error);
+       bulmaToast({
+        message: error.response ? error.response.data : error.message
+       });
     }
 };
+
+export const removeOrderItem = itemId => async(dispatch) => {
+    try{
+        const token = window.localStorage.getItem('token');
+        if(token){
+            await axios.delete(`/api/orders/${itemId}`, {
+                headers: {
+                    authorization: token,
+                },
+            });
+            dispatch(removeItem(itemId));
+        } else {
+            const order = window.localStorage.getItem('order');
+            const newOrder = JSON.parse(order);
+            newOrder.lineItems = newOrder.lineItems.filter(item => item.id !== itemId);
+            localStorage.order = JSON.stringify(newOrder);
+            dispatch(removeItem(itemId));
+        }
+    }catch(error){
+        console.error(error);
+    }
+};
+
+export const updateQuantity = (itemId, num) => async(dispatch) => {
+    try{
+        const token = window.localStorage.getItem('token');
+        if(token){
+            await axios.put(`/orders/qty`, { itemId, num }, {
+                headers: {
+                    authorization: token,
+                },
+            });
+            dispatch(_updateQuantity(itemId,num));
+        }else{
+            const order = window.localStorage.getItem('order');
+            const newOrder = JSON.parse(order);
+            if(num < 1) throw new Error('Quantity cannot be less than 1');
+            if(num > newOrder.lineItems[itemId -1].product.stock)
+                throw new Error('Quantity cannot be more than amount in stock');
+            newOrder.lineItems[itemId - 1].quantity = num;
+            localStorage.order = JSON.stringify(newOrder);
+            dispatch(_updateQuantity(itemId, num));
+        }
+    }catch(error){
+        bulmaToast({
+            message: error.response ? error.response.data : error.message
+           });
+    }
+};
+
+// export const fetchOrderData = (orderId) => async(dispatch) => {
+//     try{
+//         const { data: orderData } = await axios.get(`/api/orders/${orderId}`);
+//         dispatch(getOrderData(orderData));
+//     }catch(error){
+//         console.log("FETCH ORDER DATA ERROR", error);
+//     }
+// };
