3e3e8e8057b3128ad025867bcfb30d76682dbacd
sllozier <sarah.lozier@gmail.com>
2023-04-17 14:04:19 -0400

    home and sing album cleaned up

--- src/components/base/home/Home.js
+++ src/components/base/home/Home.js
@@ -1,52 +1,40 @@
-import React from 'react';
-import AdCarousel from './AdCarousel';
-import HomeHero from './HomeHero';
-import ViewAllAlbums from '../../albumPages/ViewAllAlbums';
-import CallToAction from './CallToAction';
-
+import React from "react";
+import AdCarousel from "./AdCarousel";
+import HomeHero from "./HomeHero";
+import ViewAllAlbums from "../../albumPages/ViewAllAlbums";
+import CallToAction from "./CallToAction";
 
 const Home = () => {
-    return (
-        <>
-        <div className='container is-family-monospace'>
-        <div className='divider is-link is-family-monospace'>NavBar Component</div>
-        <h1 className='has-background-link is-family-monospace'>Main NavBar includes:
-        <p>Home Button, Dropdown filters, Search Bar, LogIn, Cart, Sale Ribbon Placeholders</p>
-        </h1>
-        
-        
-        </div>
-        <div className='container is-fluid'>
+  return (
+    <>
+      <div className="container has-background-cream is-family-monospace"></div>
+      <div className="container is-fluid">
         <HomeHero />
-        </div>
+      </div>
 
-                <div className='divider is-warning is-family-monospace'>Featured Albums Pagination</div>
-                <h1 className='has-background-warning'>Featured Albums comp here?</h1>
-        
-                <div className='divider is-link is-family-monospace'>Ad Carousel Component</div>
-                <h1 className='has-background-link is-family-monospace'>Carousel Ads? comp</h1>
-                <div className='container is-fluid'><AdCarousel/></div>
-                
-                <div className='divider is-link is-family-monospace'>All Albums Component</div>
-                <h1 className='has-background-link is-family-monospace'>View All Albums Comp</h1>
-                <div className='container is-family-monospace'><ViewAllAlbums/></div>
-                
-                <div className='divider is-warning is-family-monospace'>Call To Action Component</div>
-                <h1 className='has-background-warning is-family-monospace'>Newsletter SignUp & Follow on Social CallToAction comp</h1>
-                <CallToAction/>
-                
-            
-           
-                <div className='divider is-warning is-family-monospace'>Footer Component</div>
-                <h1 className='has-background-warning is-family-monospace'>Footer includes: comps
-                <p>Help & FAQ: rewards prog, about, contact us, FAQ, Privacy Pol, Terms, Return Pol;
-                    My Acct: Login, Wish List, OrderHistory, Cart; Follow SM: IG, FB, Tw, Em, YouTube
-                </p>
-                </h1>
-            
-        
-        </>
-    )
-}
+      <div className="container is-fluid ">
+        <AdCarousel />
+      </div>
+
+      <div className="container is-family-monospace">
+        <ViewAllAlbums />
+      </div>
+
+      <CallToAction />
+
+      <div className="divider is-warning is-family-monospace">
+        Footer Component
+      </div>
+      <h1 className="has-background-warning is-family-monospace">
+        Footer includes: comps
+        <p>
+          Help & FAQ: rewards prog, about, contact us, FAQ, Privacy Pol, Terms,
+          Return Pol; My Acct: Login, Wish List, OrderHistory, Cart; Follow SM:
+          IG, FB, Tw, Em, YouTube
+        </p>
+      </h1>
+    </>
+  );
+};
 
 export default Home;
--- src/components/base/navbar/NavBar.js
+++ src/components/base/navbar/NavBar.js
@@ -1,135 +1,146 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { LogoutButton } from '../../auth';
-import SaleRibbon from './SaleRibbon';
+import React from "react";
+import { Link } from "react-router-dom";
+import { LogoutButton } from "../../auth";
+import SaleRibbon from "./SaleRibbon";
 
 const NavBar = () => {
-
-  document.addEventListener('DOMContentLoaded', () => {
-    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-    $navbarBurgers.forEach( el => {
-      el.addEventListener('click', () => {
+  document.addEventListener("DOMContentLoaded", () => {
+    const $navbarBurgers = Array.prototype.slice.call(
+      document.querySelectorAll(".navbar-burger"),
+      0
+    );
+    $navbarBurgers.forEach((el) => {
+      el.addEventListener("click", () => {
         const target = el.dataset.target;
         const $target = document.getElementById(target);
-        el.classList.toggle('is-active');
-        $target.classList.toggle('is-active');
-  
+        el.classList.toggle("is-active");
+        $target.classList.toggle("is-active");
       });
     });
   });
 
   return (
     <>
-    
-    <nav className='navbar is-transparent is-primary has-logo-above is-family-monospace' role="navigation" aria-label="main navigation">
-
-      <div className='navbar-brand is-family-monospace'>
-        <a className='navbar-item is-hidden-touch' href="/">
-          <img src="/piccies/waybackNavbarLogo.svg" width="280" height="70"/>
-        </a>
-        
-        <div className='navbar-item field has-addons force-center'>
-          <div className='control has-icons-right'>
-            <input className='input is-family-monospace' type="search" placeholder='search...'/>
-            <span className='icon is-small is-right'>
-              <i className="fa-solid fa-magnifying-glass"></i>
-            </span>
-          </div>
-          <div className='control '>
-            <button type="submit" className="button is-danger is-family-monospace">Search</button>
+      <nav
+        className="navbar is-transparent is-primary has-logo-above is-family-monospace"
+        role="navigation"
+        aria-label="main navigation"
+      >
+        <div className="navbar-brand ml-4 is-family-monospace">
+          <a className="navbar-item is-hidden-touch" href="/">
+            <img src="/piccies/waybackNavbarLogo.svg" width="280" height="70" />
+          </a>
+          <div className="navbar-end mr-4">
+            <div className="navbar-item field has-addons ">
+              <div className="control has-icons-right ">
+                <input
+                  className="input is-family-monospace"
+                  type="search"
+                  placeholder="search..."
+                />
+                <span className="icon is-small is-right">
+                  <i className="fa-solid fa-magnifying-glass"></i>
+                </span>
+              </div>
+              <div className="control ">
+                <button
+                  type="submit"
+                  className="button is-danger is-family-monospace"
+                >
+                  Search
+                </button>
+              </div>
+            </div>
           </div>
         </div>
-        </div>
-        <a role="button" className='navbar-burger burger' aria-label="menu" aria-expanded="false" data-target="navBarMain" href="/login">
+        <a
+          role="button"
+          className="navbar-burger burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navBarMain"
+          href="/login"
+        >
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
         </a>
-      
 
-      <div id="navBarMain" className='navbar-menu'>
-        <div className='navbar-start ml-4'>
-          <div className='navbar-item has-dropdown is-hoverable'>
-            <a className='navbar-link has-text-info'>
-              Shop By Genre
-            </a>
-              <div className='navbar-dropdown'>
-                <a className='navbar-item' href='albums'>
+        <div id="navBarMain" className="navbar-menu">
+          <div className="navbar-start ml-4">
+            <div className="navbar-item has-dropdown is-hoverable">
+              <a className="navbar-link has-text-info">Shop By Genre</a>
+              <div className="navbar-dropdown">
+                <a className="navbar-item" href="albums">
                   Genre 1
                 </a>
-                <a className='navbar-item' href='albums'>
+                <a className="navbar-item" href="albums">
                   Genre 2
                 </a>
-                <a className='navbar-item' href='albums'>
+                <a className="navbar-item" href="albums">
                   Genre 3
                 </a>
-                <a className='navbar-item' href='albums'>
+                <a className="navbar-item" href="albums">
                   Genre 4
                 </a>
               </div>
             </div>
-            <div className='navbar-item has-dropdown is-hoverable'>
-            <a className='navbar-link has-text-info' href='/albums'>
-              Shop By Artist
-            </a>
-            <div className='navbar-dropdown'>
-                <a className='navbar-item' href='albums'>
+            <div className="navbar-item has-dropdown is-hoverable">
+              <a className="navbar-link has-text-info" href="/albums">
+                Shop By Artist
+              </a>
+              <div className="navbar-dropdown">
+                <a className="navbar-item" href="albums">
                   Artist 1
                 </a>
-                <a className='navbar-item' href='albums'>
+                <a className="navbar-item" href="albums">
                   Artist 2
                 </a>
-                <a className='navbar-item' href='albums'>
+                <a className="navbar-item" href="albums">
                   Artist 3
                 </a>
-                <a className='navbar-item' href='albums'>
+                <a className="navbar-item" href="albums">
                   Artist 4
                 </a>
               </div>
             </div>
-            <div className='navbar-item has-dropdown is-hoverable'>
-            <a className='navbar-link has-text-info' href='/albums'>
-              Shop By Title
-            </a>
-            <div className='navbar-dropdown'>
-                <a className='navbar-item' href='albums'>
+            <div className="navbar-item has-dropdown is-hoverable">
+              <a className="navbar-link has-text-info" href="/albums">
+                Shop By Title
+              </a>
+              <div className="navbar-dropdown">
+                <a className="navbar-item" href="albums">
                   Title 1
                 </a>
-                <a className='navbar-item' href='albums'>
+                <a className="navbar-item" href="albums">
                   Title 2
                 </a>
-                <a className='navbar-item' href='albums'>
+                <a className="navbar-item" href="albums">
                   Title 3
                 </a>
-                <a className='navbar-item' href='albums'>
+                <a className="navbar-item" href="albums">
                   Title 4
                 </a>
               </div>
+            </div>
           </div>
-        </div>
-        <div className='navbar-end mr-4'>
-          {/* <a className='navbar-item' href="#">
+          <div className="navbar-end mr-4">
+            {/* <a className='navbar-item' href="#">
             <i className="fa-solid fa-magnifying-glass"></i>
           </a> */}
-          <span className='icon is-medium mr-2'>
-          <a className='navbar-item' href="/cart">
-              <i className="has-text-info fa-solid fa-cart-shopping fas fa-lg"></i>
-          </a>
-          </span>
-          <span className='icon is-medium ml-2'>
-          <a className='navbar-item' href="/login">
-              <i className="has-text-info fa-solid fa-right-to-bracket fas fa-lg"></i>
-          </a>
-          </span>    
-           
-          
+            <span className="icon is-medium mr-2">
+              <a className="navbar-item" href="/cart">
+                <i className="has-text-info fa-solid fa-cart-shopping fas fa-lg"></i>
+              </a>
+            </span>
+            <span className="icon is-medium ml-2">
+              <a className="navbar-item" href="/login">
+                <i className="has-text-info fa-solid fa-right-to-bracket fas fa-lg"></i>
+              </a>
+            </span>
+          </div>
         </div>
-      </div>
-      
-    </nav>
-   
-
-
+      </nav>
 
       {/* <div className="nav-container">
         <div>
@@ -184,6 +195,6 @@ const NavBar = () => {
       </div> */}
     </>
   );
-}
+};
 
 export default NavBar;
