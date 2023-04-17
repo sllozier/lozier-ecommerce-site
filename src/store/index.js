df701bcab00723e5793087c6ca78df0041ce81ac
sllozier <sarah.lozier@gmail.com>
2023-04-17 11:14:00 -0400

    fixed dependencies

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,4 @@
-import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import logger from "redux-logger";
 import accountReducer from "./reducers/accountSlice";
 import adminReducer from "./reducers/adminSlice";
@@ -8,42 +8,42 @@ import cartReducer from "./reducers/cartSlice";
 import orderReducer from "./reducers/orderSlice";
 import genreReducer from "./reducers/genreSlice";
 import {
-    persistReducer,
-    persistStore,
-    FLUSH,
-    REHYDRATE,
-    PAUSE,
-    PERSIST,
-    PURGE,
-    REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
- const rootReducer = combineReducers ({
-       account: accountReducer,
-       admin: adminReducer,
-       album: albumReducer,
-       auth: authReducer,
-       cart: cartReducer,
-       order: orderReducer,
-       genre: genreReducer,
+const rootReducer = combineReducers({
+  account: accountReducer,
+  admin: adminReducer,
+  album: albumReducer,
+  auth: authReducer,
+  cart: cartReducer,
+  order: orderReducer,
+  genre: genreReducer,
 });
 
 const persistConfig = {
-    key: 'root',
-    storage,
+  key: "root",
+  storage,
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-    reducer: persistedReducer,
-    middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware({
-            serializableCheck: {
-                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-            },
-        }).concat(logger),
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(logger),
 });
 
 export const persistor = persistStore(store);
